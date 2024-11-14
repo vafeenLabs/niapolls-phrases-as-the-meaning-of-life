a6d9e5e436b635219fabcdb574221e99797b0410
Vafeen <666av6@gmail.com>
2024-11-14 05:30:04 +0300

    Add WITH LOVE BY VAFEEN to project window name

--- src/main/kotlin/ru/vafeen/ProjectInfo.kt
+++ src/main/kotlin/ru/vafeen/ProjectInfo.kt
@@ -1,5 +1,5 @@
 package ru.vafeen
 
 internal object ProjectInfo {
-    const val NAME = "Niapoll's phrases as the meaning of life"
+    const val NAME = "Niapoll's phrases as the meaning of life WITH LOVE BY VAFEEN"
 }
